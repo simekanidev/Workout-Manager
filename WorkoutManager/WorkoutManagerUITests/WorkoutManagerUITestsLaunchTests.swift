ad5c16432695293f1d35647eb9605a1c3f41ce37
simekanidvt <100220689+simekanidvt@users.noreply.github.com>
2022-02-23 20:49:22 +0200

    Swift Lint amendments made

--- WorkoutManager/WorkoutManagerUITests/WorkoutManagerUITestsLaunchTests.swift
+++ WorkoutManager/WorkoutManagerUITests/WorkoutManagerUITestsLaunchTests.swift
@@ -22,7 +22,7 @@ class WorkoutManagerUITestsLaunchTests: XCTestCase {
         app.launch()
 
         // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test account or navigating somewhere in the app
+        // Such as logging into a test account or navigating somewhere in the app
 
         let attachment = XCTAttachment(screenshot: app.screenshot())
         attachment.name = "Launch Screen"
