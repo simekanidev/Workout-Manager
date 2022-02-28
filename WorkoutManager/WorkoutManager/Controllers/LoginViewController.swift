8bd1c69d32eb5ea54907a727e083cfbd0dec26cf
simekanidvt <100220689+simekanidvt@users.noreply.github.com>
2022-02-28 11:39:39 +0200

    Login Navigates to landingpage

---
+++ WorkoutManager/WorkoutManager/Controllers/LoginViewController.swift
@@ -0,0 +1,49 @@
+//
+//  LoginViewController.swift
+//  WorkoutManager
+//
+//  Created by Simekani Mabambi on 2022/02/28.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userNameField.delegate = self
+        passwordField.delegate = self
+       
+    }
+
+    @IBAction func enterTapped(_ sender: Any) {
+        
+        guard let password = passwordField.text  else {
+            return
+        }
+        
+        guard let username = userNameField.text  else {
+            return
+        }
+        
+        if username == "Admin" && password == "TestPass123" {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            if let viewController = storyboard.instantiateViewController(withIdentifier: "WorkoutManagerViewController") as? WorkoutManagerViewController {
+                
+                self.navigationController?.pushViewController(viewController, animated: false)
+                
+            }
+            
+        }
+        
+    }
+
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
+}
