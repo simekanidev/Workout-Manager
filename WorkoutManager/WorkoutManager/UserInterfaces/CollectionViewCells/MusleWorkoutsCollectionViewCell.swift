97246c5478fe91714cd70d1b3c7974b2a5880d4e
simekanidvt <100220689+simekanidvt@users.noreply.github.com>
2022-03-14 20:31:09 +0200

    Collection View for landing page done

---
+++ WorkoutManager/WorkoutManager/UserInterfaces/CollectionViewCells/MusleWorkoutsCollectionViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MusleWorkoutsCollectionViewCell.swift
+//  WorkoutManager
+//
+//  Created by Simekani Mabambi on 2022/03/14.
+//
+
+import UIKit
+
+class MusleWorkoutsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var muscleImage: UIImageView!
+    @IBOutlet weak var muscleName: UILabel!
+    static let identifier = "MusleWorkoutsCollectionViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func setCellProperties(image:UIImage, muscleName:String){
+        self.muscleImage.image = image
+        self.muscleName.text = muscleName
+    }
+    
+    func nib()->UINib{
+        return UINib(nibName: "MusleWorkoutsCollectionViewCell", bundle: nil)
+    }
+
+}
